f6a205dcd7cccce3d0a69b7b44253fd61419b448
nemanjavu <nemanjavu@gmail.com>
2018-12-17 14:38:52 +0100

    Sections page test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,22 +9,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flashcards/main.dart';
+import 'package:flashcards/sections/sections_pager.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  //we should add sections repository before being able to test it
+  testWidgets('Sections Test', (WidgetTester tester) async {
+    await tester.pumpWidget(Directionality(textDirection: TextDirection.ltr,
+        child: MyApp()));
+    expect(find.text('Test1'), findsOneWidget);
+    expect(find.text('Test2'), findsNothing);
+//    await tester.drag(find.byType(SectionPage), const Offset(-50, 0));
+//    expect(find.text('Test1'), findsOneWidget);
+//    expect(find.text('Test1'), findsNothing);
   });
 }
